29caf5fc83b2516913eee3d741ef4092a9b7d2d6
Francisco Silva <silva.fass@gmail.com>
2026-01-08 23:29:27 -0300

    Add the initial replication logic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use acsync::CustomError;
 use acsync::fs::FileSearcher;
 use acsync::{
     cli_helper::{self, Arg, ArgsParser},
     create_args_parser,
 };
+use std::path::PathBuf;
 use std::time::Instant;
 
 create_args_parser! {
@@ -42,12 +42,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             dryrun,
             ..
         } => {
-            if let (None, ..) | (.., None) = (origin, destination) {
-                Err(CustomError::InvalidInput(
-                    "Origin and destination arguments must be informed!",
-                ))?;
-            }
-
             if back.unwrap_or_default() {
                 println!("Syncing back...");
             }
@@ -55,12 +49,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 println!("Dry run mode...");
             }
 
-            let paths_iter = FileSearcher::new(origin.as_ref().unwrap())
+            let origin = origin.as_ref().ok_or("Origin argument must be informed!")?;
+            let destination = destination
+                .as_ref()
+                .ok_or("Destination argument must be informed!")?;
+            let paths_iter = FileSearcher::new(origin)
                 .into_iter()
                 .filter_map(|result| result.ok());
 
             let mut file_count = 0;
-            for _ in paths_iter {
+            for origin_path in paths_iter {
+                let relative_path = origin_path.strip_prefix(origin)?;
+                let destination_path = PathBuf::from(destination).join(relative_path);
+
+                if destination_path.exists() {
+                    println!("File {} already exists", destination_path.display());
+                } else if origin_path.is_dir() {
+                    println!("Creating directory {} ...", destination_path.display());
+                    std::fs::DirBuilder::new().create(&destination_path)?;
+                } else {
+                    println!("Copying file {} ...", relative_path.display());
+                    std::fs::copy(&origin_path, &destination_path)?;
+                }
                 file_count += 1;
             }
             println!("files found: {file_count}");
